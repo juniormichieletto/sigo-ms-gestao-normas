903a541c636f09d13cae06ffac85fd3941c50a7c
Junior Michieletto <juniormichieletto@gmail.com>
2021-03-21 21:09:38 +0100

    Fix typo in workflows/ci.yml

--- scripts/mvn-build-container.bat
+++ scripts/mvn-build-container.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..
-mvn clean package clean package -Dquarkus.container-image.build=true
+mvn clean package clean package -Dquarkus.container-image.build=true -Dquarkus.container-image.group=juniormichieletto 
 pause
