88fe59121dcb37afe99e69b189abae752ebcfeab
Junior Michieletto <juniormichieletto@gmail.com>
2021-03-25 02:34:10 +0100

    Fix docker step in ci workflow

--- scripts/mvn-build-container.bat
+++ scripts/mvn-build-container.bat
@@ -1,4 +1,4 @@
 @echo off
 cd ..
-mvn clean package clean package -Dquarkus.container-image.build=true -Dquarkus.container-image.group=juniormichieletto 
+mvn clean package clean package -Dquarkus.container-image.build=true -Dquarkus.container-image.group=juniormichieletto -Dquarkus.container-image.tag=latest
 pause
